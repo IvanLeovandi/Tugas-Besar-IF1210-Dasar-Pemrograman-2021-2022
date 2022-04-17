11101d8b24964a22c248a977fba9b41f706882d1
IvanLeovandi <103516915+IvanLeovandi@users.noreply.github.com>
2022-04-18 00:10:53 +0700

    debugging fungsi buy

--- Source Code/buy.py
+++ Source Code/buy.py
@@ -3,32 +3,43 @@ import datetime
 date = datetime.datetime.now()
 id_game = input('Masukkan ID Game: ')
 
-def buy (role, id_game, user_id, saldo, game, kepemilikan):
+def buy (role, id_game, user_id, saldo, game, kepemilikan, history):
     if role == 'User':
-        # history = [open("riwayat.csv")]
+        found = False
+
+        for line in history:
+            if (id_game == line[0]) and (user_id == line[3]):
+                print("Anda sudah memiliki Game tersebut!")
+                found = True
+                return history, saldo
+
         for line in kepemilikan :
             if (id_game == line[0]) and (user_id == line[1]):
                 print("Anda sudah memiliki Game tersebut!")
-                break
+                found = True
+                return history, saldo
             else:
                 for line in game:
                     if (id_game == line[0]):
-                        if (line[5] > 0):
-                            if saldo > line[4]:
-                                saldo -= line[4]
+                        if (int(line[5]) > 0):
+                            if saldo > int(line[4]):
+                                new_saldo = saldo - int(line[4])
                                 print("Game", line[1], "berhasil dibeli!")
-                                history = [line[0],line[1],line[4],user_id,date.year]
-                                return history, saldo # ini nanti harus dipisahin lagi waktu di main programnya
+                                history = f_append(history,[line[0],line[1],line[4],user_id,date.year])
+                                found = True
+                                line[5] = int(line[5]) - 1
+                                return history, new_saldo # ini nanti harus dipisahin lagi waktu di main programnya
                                 # https://note.nkmk.me/en/python-function-return-multiple-values/#:~:text=Return%20multiple%20values%20using%20commas,the%20return%20%2C%20separated%20by%20commas.
                             else:
                                 print("Saldo anda tidak cukup untuk membeli game tersebut")
-                                break
+                                found = True
+                                return history, saldo
                         else:
                             print("Stok Game tersebut sedang habis!")
-                            break
-                    else:
-                        print("Tidak ada Game dengan ID tersebut")
-                        break
-
+                            found = True
+                            return history, saldo
+                if not found :
+                    print("Tidak ada Game dengan ID tersebut")
+                    return history, saldo
     else:
         print("Anda tidak memiliki akses pada menu ini.")
