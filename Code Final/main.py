8fbf1712bef03d6419b85b2eb9dfa90a607e1140
IvanLeovandi <103516915+IvanLeovandi@users.noreply.github.com>
2022-04-23 17:28:40 +0700

    Update main.py

--- Code Final/main.py
+++ Code Final/main.py
@@ -19,12 +19,12 @@ import datetime; import time; import support ; import function
 
 
 # INISIALISASI
-user =[]; game = []; riwayat = []; kepemilikan = []; # temp_history = []
+user =[]; game = []; riwayat = []; kepemilikan = []
 user_id = ""
 
-command_user = ['list_game_toko', 'buy_game', 'list_game', 'search_my_game', 'search_game_at_store', 'topup', 'riwayat', 'save', 'exit']
-command_admin = ['register', 'tambah_game', 'ubah_game', 'ubah_stok', 'list_game_toko', 'search_game_at_store', 'topup', 'save', 'exit']
-             
+list_command = ['register', 'tambah_game', 'ubah_game', 'ubah_stok', 'list_game_toko', 'buy_game', 'list_game', 
+               'search_my_game', 'search_game_at_store', 'topup', 'riwayat', 'save', 'exit']  
+
 program = True
 hasLogin = False
 role = ""
@@ -52,7 +52,6 @@ if loaded:
         elif command == "login":
             if hasLogin:
                 print("Anda sudah login, exit terlebih dahulu untuk menggunakan akun lain")
-                print()
             else:
                 result = function.login(user)
                 user_id = result[0]
@@ -69,26 +68,26 @@ if loaded:
                     if role == "Admin":
                         user = function.register_user(user)
                     else:
-                        print("Maaf, Anda tidak memiliki akses pada menu ini")
-                        print()
+                        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.") 
+                        print("Mintalah ke administrator untuk melakukan hal tersebut.")
                 elif command == "tambah_game":
                     if role == "Admin":
                         game = function.add_game(game)
                     else:
-                        print("Maaf, Anda tidak memiliki akses pada menu ini")
-                        print()
+                        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.") 
+                        print("Mintalah ke administrator untuk melakukan hal tersebut.")
                 elif command == "ubah_game":
                     if role == "Admin":
                         function.ubah_game(game)
                     else:
-                        print("Maaf, Anda tidak memiliki akses pada menu ini")
-                        print()
+                        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.") 
+                        print("Mintalah ke administrator untuk melakukan hal tersebut.")
                 elif command == "ubah_stok":
                     if role == "Admin":
                         function.update_stok(role,game)
                     else:
-                        print("Maaf, Anda tidak memiliki akses pada menu ini")
-                        print()
+                        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.") 
+                        print("Mintalah ke administrator untuk melakukan hal tersebut.")
                 elif command == "list_game_toko":
                     function.list_game_toko(game)
                 elif command == "buy_game":
@@ -105,20 +104,15 @@ if loaded:
                 elif command == "topup":
                     if role == "Admin":
                         function.topup(user)
-                        # asumsi admin tidak akan mengisi saldo sesama admin
+                        # Asumsi admin tidak akan mengisi saldo sesama admin
                     else:
-                        print("Maaf, Anda tidak memiliki akses pada menu ini")
-                        print()
+                        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.") 
+                        print("Mintalah ke administrator untuk melakukan hal tersebut.")
                 elif command == "riwayat":
                     if role == "User":   
                         function.riwayat_pembelian(riwayat,user,username) # antara pake riwayat.csv atau variabel temp_history
                     else:
-                        print("Maaf, Anda tidak memiliki akses pada menu ini")
-                        print()
-                elif command == "game":
-                    print(game)
-                elif command == "user":
-                    print(user)
+                        print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
                 elif command == "help":
                     function.help(role)
                 elif command == "save":
@@ -131,15 +125,10 @@ if loaded:
                 else:
                     # Masukan salah, tidak sesuai keyword yang valid, sudah login
                     print("Input anda tidak valid")
-                    print('Maaf, anda harus login terlebih dahulu untuk mengirim perintah selain “login”')
                     function.help(role)        
-            elif support.f_search(command_user, command):
-                # Masukan benar, tetapi belum login sebagai user
-                print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
-            elif support.f_search(command_admin, command):
-                # Masukan benar, tetapi memasukkan command yang hanya dapat diakses oleh Admin
-                print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
+            elif support.f_search(list_command, command):
+                # Masukan benar, tetapi belum login
+                print('Maaf, anda harus login terlebih dahulu untuk mengirim perintah selain “login”')
             else:
                 # Masukan salah, tidak sesuai keyword yang valid, belum login
                 print("Input yang diberikan tidak tersedia")
-                print()
