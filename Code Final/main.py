a112e4b694a2ae6450e1c196e22a131d05befe6f
IvanLeovandi <103516915+IvanLeovandi@users.noreply.github.com>
2022-04-24 14:02:19 +0700

    Update main.py

--- Code Final/main.py
+++ Code Final/main.py
@@ -5,30 +5,27 @@ import datetime; import time; import support ; import function
 # KAMUS GLOBAL
 # type data_user : <id:string, username:string, nama:string, password:string, role:string, saldo:integer>
 # type data_game : <id:string, nama:string, kategori:string, tahun_rilis:string, harga:integer, stok:integer>
-# type data_riwayat : <game_id:string, nama:string, harga:integer, user_id:string, tahun_beli:string>
-# type data_kepemilikan : <game_id:string, user_id:string>
-# type data_temp_history : <game_id:string, nama:string, harga:integer, kategori:string, tahun_beli:integer>
+# type history : <id_game:string, nama:string, harga:integer, user_id:string, tahun_beli:string>
+# type data_kepemilikan : <id_game:string, user_id:string>
+# list_command : array of string
 
 # user : array of data_user
 # game : array of data_game 
-# riwayat : array of data_riwayat
+# riwayat : array of history
 # kepemilikan : array of data_kepemilikan
-# temp_history : array of data_temp_history
-
-# masih nunggu variabel lainnya
-
 
 # INISIALISASI
 user =[]; game = []; riwayat = []; kepemilikan = []
-user_id = ""
 
 list_command = ['register', 'tambah_game', 'ubah_game', 'ubah_stok', 'list_game_toko', 'buy_game', 'list_game', 
                'search_my_game', 'search_game_at_store', 'topup', 'riwayat', 'save', 'exit']  
 
+user_id = ""
+username = ""
+role = ""
+
 program = True
 hasLogin = False
-role = ""
-username = ""
 
 # Pemanggilan function load()
 loaded = False
@@ -104,7 +101,7 @@ if loaded:
                 elif command == "topup":
                     if role == "Admin":
                         function.topup(user)
-                        # Asumsi admin tidak akan mengisi saldo sesama admin
+                        # asumsi admin tidak akan mengisi saldo sesama admin
                     else:
                         print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut.") 
                         print("Mintalah ke administrator untuk melakukan hal tersebut.")
@@ -113,6 +110,8 @@ if loaded:
                         function.riwayat_pembelian(riwayat,user,username) # antara pake riwayat.csv atau variabel temp_history
                     else:
                         print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
+                elif command == "show_riwayat":
+                    print(riwayat)
                 elif command == "help":
                     function.help(role)
                 elif command == "save":
